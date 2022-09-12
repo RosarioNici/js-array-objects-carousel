767ed1d17a460e9e86cf4996b68bb38cb52653dd
Rosario Nici <rosarionici95@gmail.com>
2022-09-12 09:53:48 +0200

    only array

---
+++ file.js
@@ -0,0 +1,54 @@
+const images = [
+    {
+        url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+        title: 'Svezia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Perù',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+
+    {
+        url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+        title: 'Chile',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+        title: 'Argentina',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+    {
+        url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+        title: 'Colombia',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+    },
+];
+
+let containerCarosel = document.querySelector('.carousel-img')
+containerCarosel.innerHTML = ''
+
+let firstImg = 0;
+console.log(images[firstImg])
+
+for (let i = 0; i < images.length; i++) {
+
+    console.log(images[i])
+    let activeClass = '';
+    if (i === firstImg) {
+        activeClass = 'active';
+    }
+    console.log(activeClass)
+    containerCarosel.innerHTML += `<img class="img-fluid item ${activeClass} col-12" src="./img/${images.length.url[i]}" alt="Foto Carousel"></img>`
+}
+
+
+
+const buttonNext = document.getElementById('next')
+
+buttonNext.addEventListener('click', function () {
+
+})
